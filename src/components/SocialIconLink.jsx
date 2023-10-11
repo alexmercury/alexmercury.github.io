83cd3e78c5b388427d022e78310786af6c3da0bd
Alex Merkulov <rormercury@gmail.com>
2023-10-12 00:16:29 +0300

    Add SocialIconLink

---
+++ src/components/SocialIconLink.jsx
@@ -0,0 +1,22 @@
+import { createElement } from "react"
+import PropTypes from "prop-types"
+
+const SocialIconLink = function ({ href, icon }) {
+  return (
+    <a
+      className="p-3 text-yellow-500 border-2 border-yellow-500 rounded-full transition-all ease-out duration-500 hover:bg-yellow-500 hover:text-slate-950"
+      href={href || "/#"}
+      target="_blank"
+      rel="noreferrer"
+    >
+      {createElement(icon, { size: 18 })}
+    </a>
+  )
+}
+
+SocialIconLink.propTypes = {
+  href: PropTypes.string,
+  icon: PropTypes.object.isRequired
+}
+
+export default SocialIconLink
