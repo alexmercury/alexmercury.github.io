8a99d14b39cc8d8742a81f859087b5c799d6c664
Alex Merkulov <rormercury@gmail.com>
2023-10-11 21:36:26 +0300

    Configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      transitionDuration: {
+        "2000": "2000ms",
+      },
+      animation: {
+        "border-blink": "border-blink 1s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+      keyframes: {
+        "border-blink": {
+          "0%, 100%": { "border-color": "transparent" },
+          "70%": { "border-color": "white" },
+        }
+      }
+    },
   },
   plugins: [],
 }
